09e4b5ab23329ace5907f09e8bb34cd03cad6591
lixinqi <lixinqi000@126.com>
2024-09-27 17:51:49 +0800

    add

---
+++ babel.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    node: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            modules: false,
+            targets: {
+              node: 'current',
+            },
+          },
+        ],
+      ],
+      plugins: [
+        '@babel/plugin-syntax-dynamic-import',
+      ],
+    },
+    web: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            modules: false,
+            targets: {
+              browsers: ['last 2 versions'],
+            },
+          },
+        ],
+      ],
+      plugins: [
+        '@babel/plugin-proposal-object-rest-spread',
+        '@babel/plugin-syntax-dynamic-import',
+      ],
+    },
+  },
+}
